107213d8aaa7dd3bd1c0188869e09d0006daf8b8
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-21 18:42:47 -0400

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:1000/#product-list)
-if [ "$response" != "200" ] 
-   then
-   exit 1
-      else
-        print "TEST PASSED"
-        fi
