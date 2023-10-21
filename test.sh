50c74304584194e27a42afadcde5e60042b0a1a3
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-21 18:43:17 -0400

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:1000/#product-list)
+if [ "$response" != "200" ] 
+   then
+   exit 1
+      else
+        print "TEST PASSED"
+        fi
