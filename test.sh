0d7a68a24a8e9d647fd8c05f617b8a05018ebcdf
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-21 20:37:15 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,9 @@ response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:100/#produc
 if [ "$response" != "200" ] 
    then
    echo "TEST FAILED ---------------------"
+   docker stop $(docker ps -a -q)
+   docker rm $(docker ps -a -q)
+   docker rmi jorgescarenzi/ecom:$BUILD_NUMBER'
    exit 1
       else
         echo "TEST PASSED------------------"
