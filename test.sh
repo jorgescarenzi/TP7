1870a0dc77889ec1af6f3b5db926147a13259f43
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-21 20:10:07 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:1000/#product-list)
+response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:1000/#product-lit)
 if [ "$response" != "200" ] 
    then
    exit 1
