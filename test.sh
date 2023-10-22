3d6ba5ec014aee2931040ac37a9cbf2fb87e0a68
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-21 20:17:41 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,8 @@
 response=$(curl -s -o /dev/null -w "%{http_code}\n" http://localhost:100/#product-list)
 if [ "$response" != "200" ] 
    then
+   echo "TEST FAILED ---------------------"
    exit 1
       else
-        echo "TEST PASSED"
+        echo "TEST PASSED------------------"
         fi
