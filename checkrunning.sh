4fcb5744ea8d2f8dd07b8023bab332a3aaf673f0
Jorge Scarenzi <138737038+jorgescarenzi@users.noreply.github.com>
2023-10-24 12:13:39 -0400

    Update checkrunning.sh

--- checkrunning.sh
+++ checkrunning.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for id in $(docker ps -q -a -f name=ecomdbtest)
+for id in $(docker ps -q -a -f name=ecomtest)
 do
         echo "stopping container ${id}"
         docker stop "${id}"
